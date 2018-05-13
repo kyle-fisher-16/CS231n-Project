4fae81e15261d9e5701c56071b1725687327f127
Kyle Fisher <kfisher@Kyles-MacBook-Pro.local>
2018-05-12 21:34:20 -0700

    first version of dumb network (not real data)

--- network.py
+++ network.py
@@ -8,14 +8,11 @@ class DescriptorCNN(tf.keras.Sequential):
     def __init__(self, arg1):
         print(str(arg1) + '!')
         super(DescriptorCNN, self).__init__()
-        # self.input_shape = (1,)
-
-        # self.W = tf.keras.backend.variable([8.0],name="W")
-
-        self.add( tf.keras.layers.Dense(5, activation=tf.nn.relu) )
+        self.add(tf.keras.layers.Dense( 5, activation=tf.nn.relu, input_shape=arg1))
 
 
-L2_dist = lambda x:K.abs(x[0]-x[1])
+
+#L2_dist = lambda x:K.abs(x[0]-x[1])
 
 
 # fake data...
@@ -24,19 +21,22 @@ left_in = tf.keras.layers.Input(input_shape)
 right_in = tf.keras.layers.Input(input_shape)
 
 my_net = DescriptorCNN((1,));
-
 left_out = my_net(left_in);
-# right_out = my_net(right_in);
-# both_out = keras.layers.merge([left_out, right_out], output_shape=lambda x: x[0]);
+right_out = my_net(right_in);
+
+
+
+
+#siamese_net = Model(input=[left_input,right_input],output=prediction)
+
+
+
 #
+## silly test
+#fake_data_left = [1.0];
+#fake_data_right = [2.0];
+#fake_label = [0];
+#my_net.compile("Adam", loss="categorical_crossentropy")
+#out = my_net.predict(x=fake_data_left);
+#print(out)
 
-
-
-
-# silly test
-fake_data_left = [1.0];
-fake_data_right = [2.0];
-fake_label = [0];
-my_net.compile("Adam", loss="categorical_crossentropy")
-out = my_net.predict(x=fake_data_left);
-print(out)

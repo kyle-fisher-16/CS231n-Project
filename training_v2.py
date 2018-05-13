5fe81f5eafe1589ad0d0326386940d8f017e6d50
fujinam2 <fujinam2@github.com>
2018-05-13 16:18:57 -0700

    trying to make l2 regularizer for dense layer weights work but getting errors

--- training_v2.py
+++ training_v2.py
@@ -11,13 +11,15 @@ import os
 import h5py
 from dataset import Dataset
 
+import keras
+
 # Global Constants
 IMG_W = 64;
 IMG_H = 64;
 
 # ====== HYPERPARAMS ======
 batch_sz = 10;
-num_epochs = 10;
+num_steps = 10;
 
 
 # ====== LOAD DATA ======
@@ -28,13 +30,15 @@ training_dset = Dataset(data, batch_size=batch_sz);
 # ====== NETWORK ARCHITECTURE ======
 convnet = Sequential()
 convnet.add(Flatten(input_shape=(IMG_W, IMG_H)))
-convnet.add(Dense(3, activation='relu', input_shape=(2,IMG_W,IMG_H), kernel_initializer='random_uniform', bias_initializer=Constant(value=0)))
+convnet.add(Dense(3, activation='relu', input_shape=(2,IMG_W,IMG_H), kernel_initializer='random_uniform', bias_initializer=Constant(value=0),
+kernel_regularizer=keras.regularizers.l1))
 
 
 # ====== SIAMESE NETWORK ======
 siamese_input = Input((2,IMG_W,IMG_H))
-left_input = Lambda(lambda x: x[:, 0, :, :], output_shape=(batch_sz,IMG_W,IMG_H),)(siamese_input);
-right_input = Lambda(lambda x: x[:, 1, :, :], output_shape=(batch_sz,IMG_W,IMG_H),)(siamese_input);
+norm_input = keras.layers.BatchNormalization()(siamese_input)
+left_input = Lambda(lambda x: x[:, 0, :, :], output_shape=(batch_sz,IMG_W,IMG_H),)(norm_input);
+right_input = Lambda(lambda x: x[:, 1, :, :], output_shape=(batch_sz,IMG_W,IMG_H),)(norm_input);
 encoded_l = convnet(left_input)
 encoded_r = convnet(right_input)
 # calculate the loss
@@ -49,11 +53,11 @@ def loss_func(y_true, y_pred):
   # y_pred - is distance squared from siamese network
   # Here, we calculate:
   # (y_true * dist) + (1 - ytrue) * max(0, C - dist)
-  
+
   # y_true == 1:
   dist = K.sqrt(y_pred);
   match_term = multiply([dist, y_true]); # y_true * dist
-  
+
   # y_true == 0:
   C = K.constant([10.0]); # C = 10
   zero_const = K.constant([0.0]); # zero = 0
@@ -62,7 +66,7 @@ def loss_func(y_true, y_pred):
   temp1 = maximum([zero_const, c_minus_dist]); # max(0, C - dist)
   nonmatch_term_coeff = subtract([one_const, y_true]); # (1 - ytrue)
   nonmatch_term = multiply([nonmatch_term_coeff, temp1]); # (1 - ytrue) * max(0, C - dist)
-  
+
   return add([match_term, nonmatch_term]);
 
 
@@ -70,14 +74,16 @@ def loss_func(y_true, y_pred):
 siamese_net = Model(output=dist_sq, inputs=siamese_input)
 optimizer = Adam(0.01) # default was 0.00006
 siamese_net.compile(loss=loss_func,optimizer=optimizer)
-epoch = 1;
+weights = siamese_net.get_weights()
+print "init weights: ", weights
+step = 1;
 for X_batch, y_batch in training_dset:
   # Run the graph on a batch of training data; recall that asking
   # TensorFlow to evaluate loss will cause an SGD step to happen.
   loss = siamese_net.train_on_batch(x=X_batch, y=y_batch)
-  print 'Epoch #', epoch, ', loss=', loss
-  epoch += 1
-  if epoch > num_epochs:
+  weights = siamese_net.get_weights()
+  print weights
+  print 'Step #', step, ', loss=', loss
+  step += 1
+  if step > 2: # num_steps:
     break;
-
-

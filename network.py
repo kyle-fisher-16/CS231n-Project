4a95cf70efa78f675704cc8f5b10c533d5c980cd
fujinam2 <fujinam2@github.com>
2018-05-12 18:38:54 -0700

    fix to make it run in python2

--- network.py
+++ network.py
@@ -7,7 +7,7 @@ from keras import backend as K
 class DescriptorCNN(tf.keras.Sequential):
     def __init__(self, arg1):
         print(str(arg1) + '!')
-        super().__init__()
+        super(DescriptorCNN, self).__init__()
         # self.input_shape = (1,)
 
         # self.W = tf.keras.backend.variable([8.0],name="W")
@@ -26,9 +26,9 @@ right_in = tf.keras.layers.Input(input_shape)
 my_net = DescriptorCNN((1,));
 
 left_out = my_net(left_in);
-right_out = my_net(right_in);
-both_out = keras.layers.merge([left_out, right_out], output_shape=lambda x: x[0]);
-
+# right_out = my_net(right_in);
+# both_out = keras.layers.merge([left_out, right_out], output_shape=lambda x: x[0]);
+#
 
 
 
@@ -38,5 +38,5 @@ fake_data_left = [1.0];
 fake_data_right = [2.0];
 fake_label = [0];
 my_net.compile("Adam", loss="categorical_crossentropy")
-out = my_net.predict(x=fake_data);
+out = my_net.predict(x=fake_data_left);
 print(out)

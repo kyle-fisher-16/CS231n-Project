7ca1f00501ab4cc4798a98b6fe3e4867cb0a8a5d
fujinam2 <fujinam2@github.com>
2018-05-13 22:24:38 -0700

    still trying to debug nan loss and weights

--- training_v2.py
+++ training_v2.py
@@ -30,13 +30,15 @@ training_dset = Dataset(data, batch_size=batch_sz);
 # ====== NETWORK ARCHITECTURE ======
 convnet = Sequential()
 convnet.add(Flatten(input_shape=(IMG_W, IMG_H)))
-convnet.add(Dense(3, activation='relu', input_shape=(2,IMG_W,IMG_H), kernel_initializer='random_uniform', bias_initializer=Constant(value=0),
-kernel_regularizer=keras.regularizers.l1))
+# convnet.add(keras.layers.BatchNormalization(axis=1, batch_input_shape=(IMG_W*IMG_H,)))
+convnet.add(Dense(1, activation='relu', input_shape=(IMG_W*IMG_H,),      kernel_initializer='random_uniform', bias_initializer=Constant(value=0),
+kernel_regularizer=keras.regularizers.l2(0.1), bias_regularizer=keras.regularizers.l2(0.1)))
 
 
 # ====== SIAMESE NETWORK ======
 siamese_input = Input((2,IMG_W,IMG_H))
-norm_input = keras.layers.BatchNormalization()(siamese_input)
+norm_input = siamese_input
+# norm_input = keras.layers.BatchNormalization()(siamese_input)
 left_input = Lambda(lambda x: x[:, 0, :, :], output_shape=(batch_sz,IMG_W,IMG_H),)(norm_input);
 right_input = Lambda(lambda x: x[:, 1, :, :], output_shape=(batch_sz,IMG_W,IMG_H),)(norm_input);
 encoded_l = convnet(left_input)
@@ -72,18 +74,22 @@ def loss_func(y_true, y_pred):
 
 # ====== TRAINING ======
 siamese_net = Model(output=dist_sq, inputs=siamese_input)
-optimizer = Adam(0.01) # default was 0.00006
+optimizer = SGD(0.000001) # Adam(0.01) # default was 0.00006
 siamese_net.compile(loss=loss_func,optimizer=optimizer)
 weights = siamese_net.get_weights()
+print "num weights: ", len(weights)
+# print weights[0].shape
+# print weights[4].shape, weights[5].shape
 print "init weights: ", weights
 step = 1;
 for X_batch, y_batch in training_dset:
+
   # Run the graph on a batch of training data; recall that asking
   # TensorFlow to evaluate loss will cause an SGD step to happen.
   loss = siamese_net.train_on_batch(x=X_batch, y=y_batch)
   weights = siamese_net.get_weights()
-  print weights
+  print "weights at step ", step, ": ", weights
   print 'Step #', step, ', loss=', loss
   step += 1
-  if step > 2: # num_steps:
+  if step > 5: # num_steps:
     break;

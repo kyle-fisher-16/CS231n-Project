cada794d8f530c7f63c32e60f97db4f535e76a0c
fujinam2 <fujinam2@github.com>
2018-05-14 00:03:59 -0700

    trying to change everything to use tf keras instead of keras

--- training_v2.py
+++ training_v2.py
@@ -1,17 +1,32 @@
-from keras.layers import Input, Conv2D, Lambda, Flatten, merge, multiply, maximum, subtract, add, Dense, dot, Flatten,MaxPooling2D
-from keras.models import Model, Sequential
-from keras.regularizers import l2
-from keras.initializers import Constant
-from keras import backend as K
-from keras.optimizers import SGD,Adam
-from keras.losses import binary_crossentropy
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.python.keras import layers as L
+from tensorflow.python.keras import models as M
+from tensorflow.python.keras import regularizers as R
+
+
+#
+#from layers import Input, Conv2D, Lambda, Flatten, merge, multiply, maximum, subtract, add, Dense, dot, Flatten,MaxPooling2D
+#from keras import models
+#from models import Model, Sequential
+#from keras import regularizers
+
+
+# from keras.layers import Input, Conv2D, Lambda, Flatten, merge, multiply, maximum, subtract, add, Dense, dot, Flatten,MaxPooling2D
+#from keras.models import Model, Sequential
+#from keras.regularizers import l2
+#from keras.initializers import Constant
+#from keras import backend as K
+#from keras.optimizers import SGD,Adam
+#from keras.losses import binary_crossentropy
 import numpy.random as rng
 import numpy as np
 import os
 import h5py
+
 from dataset import Dataset
 
-import keras
+# import keras
 
 # Global Constants
 IMG_W = 64;
@@ -28,25 +43,28 @@ training_dset = Dataset(data, batch_size=batch_sz);
 
 
 # ====== NETWORK ARCHITECTURE ======
-convnet = Sequential()
-convnet.add(Flatten(input_shape=(IMG_W, IMG_H)))
+convnet = M.Sequential()
+convnet.add(L.Flatten(input_shape=(IMG_W, IMG_H)))
 # convnet.add(keras.layers.BatchNormalization(axis=1, batch_input_shape=(IMG_W*IMG_H,)))
-convnet.add(Dense(1, activation='relu', input_shape=(IMG_W*IMG_H,),      kernel_initializer='random_uniform', bias_initializer=Constant(value=0),
-kernel_regularizer=keras.regularizers.l2(0.1), bias_regularizer=keras.regularizers.l2(0.1)))
+convnet.add(L.Dense(1, activation='relu', input_shape=(IMG_W*IMG_H,), kernel_initializer='random_uniform', bias_initializer=keras.initializers.Constant(value=0), kernel_regularizer=keras.regularizers.l2(0.1), bias_regularizer=keras.regularizers.l2(0.1)))
 
 
 # ====== SIAMESE NETWORK ======
-siamese_input = Input((2,IMG_W,IMG_H))
+siamese_input = L.Input((2,IMG_W,IMG_H))
 norm_input = siamese_input
 # norm_input = keras.layers.BatchNormalization()(siamese_input)
-left_input = Lambda(lambda x: x[:, 0, :, :], output_shape=(batch_sz,IMG_W,IMG_H),)(norm_input);
-right_input = Lambda(lambda x: x[:, 1, :, :], output_shape=(batch_sz,IMG_W,IMG_H),)(norm_input);
+left_input = L.Lambda(lambda x: x[:, 0, :, :], output_shape=(batch_sz,IMG_W,IMG_H),)(norm_input);
+right_input = L.Lambda(lambda x: x[:, 1, :, :], output_shape=(batch_sz,IMG_W,IMG_H),)(norm_input);
 encoded_l = convnet(left_input)
 encoded_r = convnet(right_input)
 # calculate the loss
-abs_diff_lambda = lambda x: K.abs(x[0]-x[1])
-abs_diff = merge([encoded_l, encoded_r], mode = abs_diff_lambda, output_shape=lambda x: x[0])
-dist_sq = dot([abs_diff, abs_diff], axes=1, normalize=False)
+diff = tf.subtract(encoded_l, encoded_r)
+dist_sq = L.dot([diff, diff], axes=1, normalize=False)
+
+#abs_diff_lambda = lambda x: K.abs(x[0]-x[1])
+#abs_diff = L.merge([encoded_l, encoded_r], mode = abs_diff_lambda, output_shape=lambda x: x[0])
+#dist_sq = L.dot([abs_diff, abs_diff], axes=1, normalize=False)
+#tf.Print(dist_sq, [dist_sq], message="dist_sq:")
 
 
 # ====== LOSS FUNCTION ======
@@ -73,7 +91,7 @@ def loss_func(y_true, y_pred):
 
 
 # ====== TRAINING ======
-siamese_net = Model(output=dist_sq, inputs=siamese_input)
+siamese_net = M.Model(outputs=dist_sq, inputs=siamese_input) ## OUTPUT IS HERE+ ========
 optimizer = SGD(0.000001) # Adam(0.01) # default was 0.00006
 siamese_net.compile(loss=loss_func,optimizer=optimizer)
 weights = siamese_net.get_weights()
@@ -83,13 +101,12 @@ print "num weights: ", len(weights)
 print "init weights: ", weights
 step = 1;
 for X_batch, y_batch in training_dset:
-
-  # Run the graph on a batch of training data; recall that asking
-  # TensorFlow to evaluate loss will cause an SGD step to happen.
   loss = siamese_net.train_on_batch(x=X_batch, y=y_batch)
-  weights = siamese_net.get_weights()
-  print "weights at step ", step, ": ", weights
+#  weights = siamese_net.get_weights()
+#  print "weights at step ", step, ": ", weights
   print 'Step #', step, ', loss=', loss
   step += 1
-  if step > 5: # num_steps:
+  if step > 500: # num_steps:
     break;
+
+
